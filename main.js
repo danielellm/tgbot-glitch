733bb443c96e0b7d796289af13442b721a97b9c0
danielellm <ellm28@gmail.com>
2021-05-29 12:53:09 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,39 @@
-const Telegraf = require('telegraf')
-
+const { Telegraf } = require('telegraf')
+   
 // Bot TOKEN
+
+
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
-const appName = process.env.PROJECT_NAME
-const appPort = process.env.PORT
-
-// Set Webhook
-bot.telegram.setWebhook(`https://${appName}.glitch.me/webhook`)
-console.log(`Listening incoming webhook on: https://${appName}.glitch.me/webhook`)
 
 
-// Start builtin Webhook
-bot.startWebhook('/webhook', null, appPort)
 
 // Listener
-bot.on('text', (ctx) => ctx.reply('Hello Human'))
+
+
+
+bot.command('help', ctx => {
+ //There's no need slash.
+
+ //Reply when /hello command was executed
+  ctx.reply('Huzzah! help');
+})
+
+//Do something when start command was executed
+bot.start(ctx => {
+//Yes, you need callback son.
+
+//Send a message when /start command has executed
+ctx.reply('Hello, Human!');
+})
+
+bot.command('hello', ctx => {
+ //There's no need slash.
+
+ //Reply when /hello command was executed
+  ctx.reply('Huzzah! hello');
+})
+
+bot.launch()
+
+
